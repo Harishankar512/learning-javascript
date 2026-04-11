c181d2ea0e6d4f80ddc9c6324f94088fa6d55759
Hari shankar <harishankarprasadcholleti@gmail.com>
2026-04-11 09:57:40 +0530

    added

--- script.js
+++ script.js
@@ -9,5 +9,12 @@ console.log(c);
 let userData=[10,"hello",false,null];
 for(let index= 0; index < userData.length;index++)
 {
-    console.log(typeof userData[index]);
+    console.log(userData[index]);
 }
+//adding and removing at last
+userData.push(20);
+userData.pop(10);
+console.log(userData);
+//adding and removing at first
+userData.shift(30);
+console.log(userData);
