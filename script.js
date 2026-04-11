1b0b428f92ee668d20690ccb0d4bed4bd05b38b4
Hari shankar <harishankarprasadcholleti@gmail.com>
2026-04-11 09:27:39 +0530

    add all files

---
+++ script.js
@@ -0,0 +1,13 @@
+//var a=45;
+var a=62;
+console.log(a);
+let b=26;
+ b=23;
+console.log(b);
+const c=29;
+console.log(c);
+let userData=[10,"hello",false,null];
+for(let index= 0; index < userData.length;index++)
+{
+    console.log(typeof userData[index]);
+}
