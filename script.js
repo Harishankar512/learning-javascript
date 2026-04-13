36dd3fbb5cbd8b3ca3027bae444d8429fefc72f7
Hari shankar <harishankarprasadcholleti@gmail.com>
2026-04-13 09:13:02 +0530

    changes done

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ console.log(userData);
 //adding and removing at first
 userData.shift(30);
 console.log(userData);
+const obj={
+     myData: [10,"hello","bye","bye"],
+    id:1,
+    name:"guest"
+}
+console.log(obj.loc);
+obj.loc="hyd"
+console.log(obj);
+console.log(obj.myData);
+const users=[
+    {
+        id:1,
+        name:"user",
+        loc:"hyd"
+    },
+    {
+        id:2,
+        name:"user2",
+        loc:"hyd2"
+    },
+]
+console.log(users[0].name);
